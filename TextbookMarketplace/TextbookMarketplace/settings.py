b7734be1e096f3ec4c8b6bbf3a98e3334c172fa5
Daniel-Ax <finywow@gmail.com>
2025-01-31 20:57:22 +0100

    added db

--- TextbookMarketplace/TextbookMarketplace/settings.py
+++ TextbookMarketplace/TextbookMarketplace/settings.py
@@ -71,12 +71,16 @@ WSGI_APPLICATION = 'TextbookMarketplace.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
+import os
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db' / 'db.sqlite3',
+        'NAME': os.path.join(BASE_DIR, "db.sqlite3"),
     }
 }
+
 
 LOGIN_URL = 'login'
 
